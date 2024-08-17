ff6ad1723c72c365faa1a84e80595957341c4af8
Joe Parish <jparish1977@gmail.com>
2024-08-17 14:55:00 -0400

    bah! no ternary in lua

--- mods/minegistics/src/settings.lua
+++ mods/minegistics/src/settings.lua
@@ -1,5 +1,5 @@
 -- Enable debug mode
 DEBUG_MODE = minetest.settings:get_bool("minegistics_debug", false)
 -- To ease debugging, allow disabling the fuel requirement
-REQUIRE_FUEL = DEBUG_MODE ? minetest.settings:get_bool("minegistics_require_fuel", true) : true
+REQUIRE_FUEL = (DEBUG_MODE and minetest.settings:get_bool("minegistics_require_fuel", true)) or true
 
